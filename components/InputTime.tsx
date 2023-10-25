3c622e706e27652e55fa395645c81b457e7f5fab
Ivan Tkachuk <116971836+hxrv0q@users.noreply.github.com>
2023-10-25 23:56:59 +0300

    styles for components

--- components/InputTime.tsx
+++ components/InputTime.tsx
@@ -1,42 +1,113 @@
 import React, { useContext, useState } from "react";
-import { Button, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { TimerContext } from "../context/TimerContext";
 
 export default function InputTime() {
-  const [workTime, setWorkTime] = useState("");
-  const [breakTime, setBreakTime] = useState("");
+  const [workMinutes, setWorkMinutes] = useState("");
+  const [workSeconds, setWorkSeconds] = useState("");
+  const [breakMinutes, setBreakMinutes] = useState("");
+  const [breakSeconds, setBreakSeconds] = useState("");
 
   const { dispatch } = useContext(TimerContext) || { dispatch: () => {} };
 
   const handleSetWorkTime = () => {
-    const workTimeInSeconds = parseInt(workTime) * 60 || 0;
+    const workTimeInSeconds = (parseInt(workMinutes) * 60 || 0) +
+      (parseInt(workSeconds) || 0);
     dispatch({ type: "SET_WORK_TIME", payload: workTimeInSeconds });
-    setWorkTime("");
+    setWorkMinutes("");
+    setWorkSeconds("");
   };
 
   const handleSetBreakTime = () => {
-    const breakTimeInSeconds = parseInt(breakTime) * 60 || 0;
+    const breakTimeInSeconds = (parseInt(breakMinutes) * 60 || 0) +
+      (parseInt(breakSeconds) || 0);
     dispatch({ type: "SET_BREAK_TIME", payload: breakTimeInSeconds });
-    setBreakTime("");
+    setBreakMinutes("");
   };
 
   return (
-    <View>
-      <TextInput
-        placeholder="Work Minutes"
-        value={workTime}
-        onChangeText={setWorkTime}
-        keyboardType="numeric"
-      />
-      <Button title="Set Work Time" onPress={handleSetWorkTime} />
+    <View style={styles.container}>
+      <View style={styles.timeInputContainer}>
+        <TextInput
+          style={[styles.input, styles.minuteInput]}
+          placeholder="Work Min"
+          value={workMinutes}
+          onChangeText={setWorkMinutes}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={[styles.input, styles.secondInput]}
+          placeholder="Work Sec"
+          value={workSeconds}
+          onChangeText={setWorkSeconds}
+          keyboardType="numeric"
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleSetWorkTime}>
+        <Text style={styles.buttonText}>Set Work Time</Text>
+      </TouchableOpacity>
 
-      <TextInput
-        placeholder="Break Minutes"
-        value={breakTime}
-        onChangeText={setBreakTime}
-        keyboardType="numeric"
-      />
-      <Button title="Set Break Time" onPress={handleSetBreakTime} />
+      <View style={styles.timeInputContainer}>
+        <TextInput
+          style={[styles.input, styles.minuteInput]}
+          placeholder="Break Min"
+          value={breakMinutes}
+          onChangeText={setBreakMinutes}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={[styles.input, styles.secondInput]}
+          placeholder="Break Sec"
+          value={breakSeconds}
+          onChangeText={setBreakSeconds}
+          keyboardType="numeric"
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleSetBreakTime}>
+        <Text style={styles.buttonText}>Set Break Time</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    padding: 16,
+  },
+  timeInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "transparent",
+    borderBottomColor: "#D1C4E9",
+    borderWidth: 1,
+    flex: 1,
+  },
+  minuteInput: {
+    marginRight: 10,
+  },
+  secondInput: {
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: "#6200EA",
+    padding: 12,
+    marginVertical: 5,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontWeight: "600",
+  },
+});
--- components/TimerDisplay.tsx
+++ components/TimerDisplay.tsx
@@ -1,16 +1,54 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { TimerContext } from "../context/TimerContext";
 
 export default function TimerDisplay() {
   const { state } = useContext(TimerContext) || { state: undefined };
 
+  const totalDuration = state?.work ? state?.workTime : state?.breakTime || 0;
   const time = state?.time || 0;
-  const timeInMMSS = `${
-    Math.floor(time / 60)
-      .toString()
-      .padStart(2, "0")
-  }:${(time % 60).toString().padStart(2, "0")}`;
+  const percentage = (time / totalDuration) * 100;
 
-  return <Text>{timeInMMSS}</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle}>
+        <View style={[styles.progressCircle, { width: `${percentage}%` }]} />
+        <Text style={styles.timeText}>
+          {Math.floor(time / 60).toString().padStart(2, "0")}:
+          {(time % 60).toString().padStart(2, "0")}
+        </Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+  },
+  circle: {
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    borderWidth: 10 / 2,
+    borderColor: "#6200EA",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    overflow: "hidden",
+  },
+  progressCircle: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    backgroundColor: "#E0E0E0",
+  },
+  timeText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    zIndex: 1,
+  },
+});
